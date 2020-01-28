56618bafaa6c5de230a26a3572e1f9d26db670ba
wenlong.yu <1357035302@qq.com>
2020-01-28 17:20:00 +0800

    异常处理器中用Sl4fj记录异常的日志信息

--- src/main/java/com/wenlong/yu/exception/GshopExceptionHandler.java
+++ src/main/java/com/wenlong/yu/exception/GshopExceptionHandler.java
@@ -2,16 +2,19 @@ package com.wenlong.yu.exception;
 
 import com.wenlong.yu.pojo.Result;
 import com.wenlong.yu.utils.ResultBuilder;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+@Slf4j
 @ControllerAdvice
 public class GshopExceptionHandler {
 
     @ExceptionHandler(GshopException.class)
     @ResponseBody
     public Result exceptionHandler(GshopException e){
+        log.error("错误消息：{}", e.getMessage(), e);
        return ResultBuilder.buildNgResult(e.getMsg());
     }
 }
